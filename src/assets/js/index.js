19639473e43d91a5dbdb31ab5b97bfc15367f8f2
Greta Fest <greta.fest@saatchi-pro.de>
2020-10-07 15:44:28 +0200

    suche - basic setup

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,7 +3,9 @@ import '../scss/index.scss'
 const deeplink = require('./deeplink')
 const scrollTop = require('./scrolltop')
 const govUK = require('./govuk')
+const search = require('./search')
 
 govUK.initAll()
+search()
 scrollTop()
 deeplink()
--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,36 +1,69 @@
-// (function(window, document) {
-//   "use strict";
+const axios = require('axios')
+const SEARCH_URL = 'https://api.rechtsinformationsportal.de/search?q='
+const RESULTS_PER_PAGE = 10
+const slugRegex = /laws\/(.*)\/article/ // regex to get slug in article url
+const origin = window.location.origin
+const searchForm = document.getElementById('search')
+const searchInput = document.getElementById('search-input')
 
-//   const search = e => {
-//     const results = window.searchIndex.search(e.target.value, { expand: true });
+const search = (e) => {
+  // fetch using query params
+  e.preventDefault()
+  const input = searchInput.value
+  const query = encodeURIComponent(input)
+  window.location.href = origin + '/suche/?query=' + query
+}
 
-//     const resEl = document.getElementById("searchResults");
-//     resEl.innerHTML = "";
-//     if (results && results.length > 0) {
-//       results.map(r => {
-//         const { id, title, excerpt } = r.doc;
-//         const el = document.createElement("li");
-//         resEl.appendChild(el);
+function displayResults(response) {
+  //display first 10 results + pagination
+  const resEl = document.getElementById('search-results')
+  const total = response.pagination.total
+  resEl.innerHTML = ''
+  response.data.map((entry) => {
+    const el = document.createElement('li')
+    resEl.appendChild(el)
+    const h2 = document.createElement('h2')
+    el.classList.add('article-name')
+    el.appendChild(h2)
+    const a = document.createElement('a')
+    // open in new tab?
+    let url = ''
+    if (entry.type === 'article') {
+      a.textContent = entry.name + ' ' + entry.title
+      const slug = entry.url.match(slugRegex)
+      url = origin + '/' + slug[1] + '/index.html#' + entry.id
+    } else if (entry.type === 'law') {
+      a.textContent = entry.titleShort || entry.titleLong
+      if (entry.titleShort) a.innerHTML += `<br/>${entry.titleLong}`
+      url = origin + '/' + entry.slug
+    }
+    console.log(url)
+    a.setAttribute('href', url)
+    h2.appendChild(a)
+  })
+}
 
-//         const h3 = document.createElement("h3");
-//         el.appendChild(h3);
+async function fetchResults(query) {
+  const url = SEARCH_URL + query
+  try {
+    const response = await axios.get(url)
+    // return the data and the total number of search results
+    displayResults(response.data)
+    return response
+  } catch (err) {
+    console.log(err)
+    return []
+  }
+}
 
-//         const a = document.createElement("a");
-//         a.setAttribute("href", id);
-//         a.textContent = title;
-//         h3.appendChild(a);
+const init = () => {
+  searchForm.onsubmit = search
+  const urlParams = new URLSearchParams(window.location.search)
+  const query = urlParams.get('query')
+  if (query) {
+    searchInput.value = decodeURIComponent(query)
+    fetchResults(query)
+  }
+}
 
-//         const p = document.createElement("p");
-//         p.textContent = excerpt;
-//         el.appendChild(p);
-//       });
-//     }
-//   };
-
-//   fetch("/search-index.json").then(response =>
-//     response.json().then(rawIndex => {
-//       window.searchIndex = elasticlunr.Index.load(rawIndex);
-//       document.getElementById("searchField").addEventListener("input", search);
-//     })
-//   );
-// })(window, document);
+module.exports = init
