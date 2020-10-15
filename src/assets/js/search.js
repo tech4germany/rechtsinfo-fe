d2ec9588295f387f2c558acde93489bfb5f7e514
Greta Fest <greta.fest@saatchi-pro.de>
2020-10-15 17:38:18 +0200

    add search filters

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,44 +1,50 @@
 const axios = require('axios')
 const config = require('../../_data/config')
 
-const SEARCH_URL = config.BASE_API_URL + '/search?q='
+const SEARCH_URL = config.BASE_API_URL + '/search'
 const RESULTS_PER_PAGE = 10
 
 const slugRegex = /laws\/(.*)\/article/ // regex to get slug in article url
 const origin = window.location.origin
+
 const searchForm = document.getElementById('search-bar')
 const searchInput = document.getElementById('search-bar__input')
-
-let page = 1
+const sections = [...document.querySelectorAll('.search-results')]
 
 // remove html tags from strings
 const removeTags = (string) => {
-  string && string.length ? string.replace(/(<([^>]+)>)/gi, '') : string
+  const clean =
+    string && string.length ? string.replace(/(<([^>]+)>)/gi, ' ') : string
+  return clean
 }
 
 const search = (e) => {
-  // fetch using query params
   e.preventDefault()
   const input = searchInput.value
   const query = encodeURIComponent(input)
-  window.location.href = origin + '/suche/?query=' + query
+  const searchURL = new URL(SEARCH_URL)
+  // add input to query params
+  searchURL.searchParams.append('q', query)
+  // navigate to search template
+  window.location.href = searchURL
 }
 
-const displayResults = (data) => {
-  //display first 10 results + pagination
-  const resEl = document.getElementById('search-results')
+const displayResults = (response, element, page) => {
+  // create markup for search results
+  const list = element.querySelector('ul')
+  const { data, pagination, links } = response
   data.map((entry) => {
     const el = document.createElement('li')
-    resEl.appendChild(el)
+    list.appendChild(el)
     const h2 = document.createElement('h2')
     el.classList.add('article-name')
     el.appendChild(h2)
     const a = document.createElement('a')
-    // open in new tab?
+    a.target = '_blank'
     let url = ''
     if (entry.type === 'article') {
-      a.textContent = entry.name
-      if (entry.title) a.textContent += ` ${entry.title}`
+      a.textContent = removeTags(entry.name)
+      if (entry.title) a.textContent += ` ${removeTags(entry.title)}`
       const slug = entry.url.match(slugRegex)
       url = origin + '/' + slug[1] + '/index.html#' + entry.id
     } else if (entry.type === 'law') {
@@ -49,33 +55,39 @@ const displayResults = (data) => {
     a.setAttribute('href', url)
     h2.appendChild(a)
   })
+  // show total pages + load more button
+  if (page === 1) {
+    showPagination(pagination, element)
+  }
+  // fetch next page
+  if (links.next) getMoreResults(links.next, element, page)
 }
 
-const showPagination = (pagination) => {
-  document.getElementById('pagination').classList.remove('hidden')
-  const totalPages = document.getElementById('total-pages')
+const showPagination = (pagination, element) => {
+  element.querySelector('.pagination').classList.remove('hidden')
+  const totalPages = element.querySelector('.total-pages')
+  const id = element.id
+  const totalResults = document.querySelector('.filter-number' + '.' + id)
   const pages = Math.ceil(pagination.total / RESULTS_PER_PAGE)
+  totalResults.innerText = pagination.total
   totalPages.innerText = pages
 }
 
-const getMoreResults = (url) => {
+const getMoreResults = (url, element, page) => {
   // TODO: show button only if page < total pages
-  const moreResultsBtn = document.getElementById('pagination-btn')
-  const currentPage = document.getElementById('current-page')
+  const moreResultsBtn = element.querySelector('.pagination-btn')
+  const currentPage = element.querySelector('.current-page')
   currentPage.innerText = page
   moreResultsBtn.onclick = () => {
     page++
-    if (url) fetchResults(url, page)
+    if (url) fetchResults(url, element, page)
   }
 }
 
-const fetchResults = async (url, page) => {
+const fetchResults = async (url, element, page) => {
   try {
     const response = await axios.get(url)
-    const { pagination, links, data } = response.data
-    if (page === 1) showPagination(pagination)
-    displayResults(data)
-    if (links.next) getMoreResults(links.next)
+    displayResults(response.data, element, page)
     return response
   } catch (err) {
     console.log(err)
@@ -88,9 +100,20 @@ const init = () => {
   const urlParams = new URLSearchParams(window.location.search)
   const query = urlParams ? urlParams.get('query') : ''
   if (query) {
-    const url = SEARCH_URL + query
+    const url = new URL(SEARCH_URL)
+    url.searchParams.append('q', query)
     searchInput.value = decodeURIComponent(query)
-    fetchResults(url, page)
+    const filterItems = sections.filter((item) => {
+      return item.dataset.filter
+    })
+    fetchResults(url, sections[0], 1).then(() => {
+      // load first page of results, then first pages of each filter
+      filterItems.map((item) => {
+        const filterUrl = new URL(url)
+        filterUrl.searchParams.append('type', item.dataset.filter)
+        fetchResults(filterUrl, item, 1)
+      })
+    })
   }
 }
 
