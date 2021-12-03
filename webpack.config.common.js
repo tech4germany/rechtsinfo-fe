3fa69e6574803439f1b5179279508208cca60daa
Niko Felger <niko.felger@gmail.com>
2021-12-03 10:23:16 +0100

    Fix dependencies to work with node 16

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,3 @@
-const Fiber = require('fibers')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
@@ -27,7 +26,6 @@ module.exports = {
             options: {
               implementation: require('sass'),
               sassOptions: {
-                fiber: Fiber,
                 outputStyle: 'expanded',
               },
             },
