de12d441c2761836d725ab43015ec391b429b552
Niko Felger <niko.felger@gmail.com>
2021-12-03 12:14:05 +0100

    Hide disclaimer for the duration of the page session

--- src/assets/js/disclaimer-modal.js
+++ src/assets/js/disclaimer-modal.js
@@ -2,9 +2,14 @@ const disclaimerModal = () => {
   const sessionStorage = window.sessionStorage
   const modal = document.getElementById('disclaimer-modal')
   const modalCloseBtn = document.getElementById('close-disclaimer-modal')
-  modal.open()
-  modalCloseBtn.onclick = () => modal.close()
 
+  if (!sessionStorage.getItem('hideDisclaimer')) {
+    modal.open()
+    modalCloseBtn.onclick = () => {
+      sessionStorage.setItem('hideDisclaimer', 'true')
+      modal.close()
+    }
+  }
 }
 
 module.exports = disclaimerModal
